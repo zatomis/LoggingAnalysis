1f877af8f4523e4a792dfe7099254e1df7ee2543
zatomis <zatomis@list.ru>
2025-04-26 19:56:20 +0300

    Сырой отчет

--- main.py
+++ main.py
@@ -15,37 +15,53 @@ class DatabaseLogs:
         self.cursor.execute('''
             CREATE TABLE logs (
                 id INTEGER PRIMARY KEY,
-                name TEXT NOT NULL
+                log_level TEXT NOT NULL,
+                api TEXT NOT NULL
             )
         ''')
-
-    def add_name(self, record):
+    def get_total_count(self):
         try:
-            self.cursor.execute('INSERT INTO logs (name) VALUES (?)', (name,))
-            self.connection.commit()
-            print(f"Имя '{name}' успешно добавлено.")
+            self.cursor.execute('SELECT COUNT(*) AS total_records FROM logs')
+            return self.cursor.fetchone()[0]
         except Exception as e:
             print(f"Ошибка при добавлении имени: {e}")
 
-    def get_all_names(self):
-        self.cursor.execute('SELECT * FROM names')
+    def add_name(self, log_level, api):
+        try:
+            self.cursor.execute('INSERT INTO logs (log_level, api) VALUES (?,?)', (log_level, api))
+            self.connection.commit()
+        except Exception as e:
+            print(f"Ошибка при добавлении имени: {e}")
+
+    def get_all(self):
+        self.cursor.execute('SELECT * FROM logs')
         return self.cursor.fetchall()
 
+    def get_report(self):
+        self.cursor.execute(
+
+            """
+            SELECT 
+            api,
+            SUM(CASE WHEN log_level = 'DEBUG' THEN 1 ELSE 0 END) AS debug,
+            SUM(CASE WHEN log_level = 'INFO' THEN 1 ELSE 0 END) AS info,
+            SUM(CASE WHEN log_level = 'WARNING' THEN 1 ELSE 0 END) AS warning,
+            SUM(CASE WHEN log_level = 'ERROR' THEN 1 ELSE 0 END) AS error,
+            SUM(CASE WHEN log_level = 'CRITICAL' THEN 1 ELSE 0 END) AS critical
+        FROM 
+            logs
+        GROUP BY 
+            api
+        ORDER BY 
+            api
+            """
+
+        )
+        return self.cursor.fetchall()
+
+
     def close(self):
-        """Закрывает соединение с базой данных."""
         self.connection.close()
-
-
-    # db = DatabaseLogs()
-    # db.add_name("Alice")
-    # db.add_name("Bob")
-    # names = db.get_all_names()
-    # print("Список имен в базе данных:")
-    # for id, name in names:
-    #     print(f"{id}: {name}")
-    # db.close()
-
-
 
 def file_exists(path):
     return os.path.isfile(path)
@@ -68,11 +84,27 @@ def parse_arguments():
     args = parser.parse_args()
     return args
 
+def get_only_api(data):
+    position1 = data.find('/')
+    position2 = len(data) - data[::-1].find('/')
+    return data[position1:position2]
+
+
+def convert_log(log_record):
+    template = 'django.request'
+    if template in log_record:
+        correct_rec = log_record.split('000')[1]
+        correct_rec = correct_rec.split(template)
+        return correct_rec[0].strip() + ' ' + get_only_api(correct_rec[1])
+    else:
+        return ''
+
+
 def read_logs_to_list(file_path):
     try:
         with open(file_path, 'r') as file:
             records = file.readlines()
-        return [rec.strip() for rec in records]
+        return [convert_log(rec) for rec in records if len(convert_log(rec))>0]
     except Exception as e:
         print(f"Произошла ошибка: {e}")
         return []
@@ -88,19 +120,26 @@ if __name__ == '__main__':
     parsed_arguments = parse_arguments()
     logging.info(f"Запуск программы {parsed_arguments.report}")
 
+    db = DatabaseLogs()
+
     try:
         for log_file in parsed_arguments.log_files:
             logging.info(f'Обрабатываем файл: {log_file}')
             if file_exists(log_file):
                 print(f"Файл '{log_file}' существует.")
                 logs_list = read_logs_to_list(log_file)
-                for line in logs_list[:5]:
-                    print(line)
-
+                for log_line in logs_list:
+                    db_rec = log_line.split(' ')
+                    # print(db_rec)
+                    db.add_name(db_rec[0], db_rec[1])
+                print(f"В базе {db.get_total_count()} записей")
+                report_result = db.get_report()
+                for row in report_result:
+                    print(
+                        f"Handler: {row[0]}, DEBUG: {row[1]}, INFO: {row[2]}, WARNING: {row[3]}, ERROR: {row[4]}, CRITICAL: {row[5]}")
             else:
                 logging.error(f"Файл '{log_file}' не найден.")
-
     except Exception as ex:
         print(f"Ошибка {ex}")
-
+    db.close()
 
