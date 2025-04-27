2e8bb3ae2d7d8a2952c7777904102361a51b3209
zatomis <zatomis@list.ru>
2025-04-27 08:35:27 +0300

    Отдельная функция для показа отчета

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import re
 from os import path
 import sqlite3
 from terminaltables import DoubleTable
@@ -26,7 +27,7 @@ class DatabaseLogs:
         except Exception as e:
             print(f"Ошибка при добавлении имени: {e}")
 
-    def add_name(self, log_level, api):
+    def add_loglevel(self, log_level, api):
         try:
             self.cursor.execute('INSERT INTO logs (log_level, api) VALUES (?,?)', (log_level, api))
             self.connection.commit()
@@ -55,13 +56,18 @@ class DatabaseLogs:
         ORDER BY 
             api
             """
-
         )
         return self.cursor.fetchall()
 
-
     def close(self):
         self.connection.close()
+
+def is_valid_name(name):
+    pattern = r'^[A-Za-z0-9]+$'
+    if re.match(pattern, name):
+        return name
+    else:
+        return ''
 
 def file_exists(path):
     return os.path.isfile(path)
@@ -109,6 +115,27 @@ def read_logs_to_list(file_path):
         print(f"Произошла ошибка: {e}")
         return []
 
+def show_table(report:list, count:int, title:str):
+    report = db.get_report()
+    table = [['HANDLER', 'DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL']]
+    sum_debug = sum_info = sum_warning = sum_error = sum_critical = 0
+    for row in report:
+        table_row = [row[0], row[1], row[2], row[3], row[4], row[5]]
+        sum_debug = sum_debug + row[1]
+        sum_info = sum_info + row[2]
+        sum_warning = sum_warning + row[3]
+        sum_error = sum_error + row[4]
+        sum_critical = sum_critical + row[5]
+        table.append(table_row)
+    table_row = ['', '', '', '', '', '']
+    table.append(table_row)
+    table_row = ['', sum_debug, sum_info, sum_warning, sum_error, sum_critical]
+    table.append(table_row)
+    table_instance = DoubleTable(table, f" Total requests: {count}. {is_valid_name(title)} ")
+    table_instance.justify_columns[2] = 'center'
+    print()
+    print(table_instance.table)
+
 
 if __name__ == '__main__':
     logging.basicConfig(
@@ -119,26 +146,21 @@ if __name__ == '__main__':
     base_dir = path.dirname(path.abspath(__file__))
     parsed_arguments = parse_arguments()
     logging.info(f"Запуск программы {parsed_arguments.report}")
-
+    os.system('clear')
     db = DatabaseLogs()
-
     try:
         for log_file in parsed_arguments.log_files:
             logging.info(f'Обрабатываем файл: {log_file}')
             if file_exists(log_file):
-                print(f"Файл '{log_file}' существует.")
                 logs_list = read_logs_to_list(log_file)
                 for log_line in logs_list:
                     db_rec = log_line.split(' ')
-                    # print(db_rec)
-                    db.add_name(db_rec[0], db_rec[1])
-                print(f"В базе {db.get_total_count()} записей")
-                report_result = db.get_report()
-                for row in report_result:
-                    print(
-                        f"Handler: {row[0]}, DEBUG: {row[1]}, INFO: {row[2]}, WARNING: {row[3]}, ERROR: {row[4]}, CRITICAL: {row[5]}")
+                    db.add_loglevel(db_rec[0], db_rec[1])
             else:
                 logging.error(f"Файл '{log_file}' не найден.")
+
+        show_table(db.get_report(), db.get_total_count(), parsed_arguments.report)
+
     except Exception as ex:
         print(f"Ошибка {ex}")
     db.close()
